0b4a1219c4fd30605fab7b6a8b16989828157fe5
Mark Johns <mark.b.johns@gmail.com>
2023-12-14 01:44:56 -0500

    finished functionality

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 const searchBar=document.querySelector('input');
 const resultsBar=document.getElementById('results-bar');
-const resultsList=document.querySelector('ul');
+const resultList=document.querySelector('ul');
+const suggestions=document.querySelectorAll('li');
 const fruits=[
 'Apple','Apricot','Avocado','Banana','Bilberry','Blackberry',
 'Blackcurrant','Blueberry','Boysenberry','Currant','Cherry','Coconut','Cranberry',
 'Cucumber','Custard apple','Damson','Date','Dragonfruit','Durian','Elderberry',
-'Feijoa','Fig','Gooseberry','Grape','Raisin','Grapefruit','Guava','Honeyberry',
+'Feijoa','Fig','Gooseberry','Grape','Grapefruit','Guava','Honeyberry',
 'Huckleberry','Jabuticaba','Jackfruit','Jambul','Juniper berry','Kiwifruit',
 'Kumquat','Lemon','Lime','Loquat','Longan','Lychee','Mango','Mangosteen',
 'Marionberry','Melon','Cantaloupe','Honeydew','Watermelon','Miracle fruit',
 'Mulberry','Nectarine','Nance','Olive','Orange','Clementine','Mandarine',
 'Tangerine','Papaya','Passionfruit','Peach','Pear','Persimmon','Plantain','Plum',
-'Pineapple','Pomegranate','Pomelo','Quince','Raspberry','Salmonberry','Rambutan',
-'Redcurrant','Salak','Satsuma','Soursop','Star fruit','Strawberry','Tamarillo',
-'Tamarind','Yuzu'
+'Pineapple','Pomegranate','Pomelo','Quince','Raisin','Raspberry','Rambutan',
+'Redcurrant','Salmonberry','Salak','Satsuma','Soursop','Star fruit','Strawberry',
+'Tamarillo','Tamarind','Yuzu'
 ];
 
-const results=[];
 function search(query){
     let searchTerm=query.toLowerCase();
     let results=[];
@@ -24,7 +24,7 @@ function search(query){
         return results;
     }
     fruits.forEach(fruitName=>{
-        if(fruitName.toLowerCase().includes(searchTerm)&&fruitName.toLowerCase().startsWith(searchTerm)){
+        if(fruitName.toLowerCase().includes(searchTerm)){
             results.push(fruitName);
         }
     });
@@ -33,11 +33,34 @@ function search(query){
 function searchHandler(e){
     let query=searchBar.value;
     let results=search(query)
-    resultsList.innerHTML='';
+    resultList.innerHTML='';
     results.forEach(result=>{
         let listItem=document.createElement('li');
         listItem.textContent=result;
-        resultsList.appendChild(listItem);
+        resultList.appendChild(listItem);
     })
 }
-searchBar.addEventListener('keyup',searchHandler);
+function showSuggestions(){
+    resultList.addEventListener('mouseover',function(e){
+        if(e.target.tagName==='LI'){
+            e.target.classList.add('hover');
+        }
+    }); 
+    resultList.addEventListener('mouseout',function(e){
+        if(e.target.tagName==='LI'){
+            e.target.classList.remove('hover');
+        }
+    });
+}
+function useSuggestion(e){
+    if(e.target.tagName==='LI'){
+        let selected=e.target.textContent;
+        searchBar.value=selected;
+        resultList.innerHTML='';
+    }
+}
+searchBar.addEventListener('keyup',function(e){
+    searchHandler(e);
+    showSuggestions();
+});
+resultList.addEventListener('click', useSuggestion);

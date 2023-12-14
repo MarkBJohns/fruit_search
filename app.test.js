17ef212788d7be47bafab9ebdd45c5863b6a87af
Mark Johns <mark.b.johns@gmail.com>
2023-12-14 02:32:07 -0500

    added a test for search()

---
+++ app.test.js
@@ -0,0 +1,9 @@
+// describe('search() to create a "results" array based on the search parameters',function(){
+//     it('should return an empty array with no parameters',function(){
+//         expect(search('')).toEqual([]);
+//     })
+//     it('should return an array with every fruit that has the parameters anywhere in the string',function(){
+//         expect(search('ap')).toEqual(['Apple','Apricot','Custard apple','Grape','Grapefruit','Papaya','Pineapple']);
+//         expect(search('apple')).toEqual(['Apple','Custard apple','Pineapple']);
+//     })
+// })
